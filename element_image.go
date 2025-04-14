f389751d76c81e284db1fdf2dd56f65e135a33eb
MarlinKuhn <marlin.kuhn@web.de>
2025-04-14 13:15:06 +0200

    update image to not go by path

--- element_image.go
+++ element_image.go
@@ -1,12 +1,15 @@
 package gipdf
 
-import "github.com/signintech/gopdf"
+import (
+	"github.com/pkg/errors"
+	"github.com/signintech/gopdf"
+)
 
 type Image struct {
-	Path   string
-	Ratio  float64
-	Width  *float64
-	Height *float64
+	GetBytes func() ([]byte, error)
+	Ratio    float64
+	Width    *float64
+	Height   *float64
 }
 
 func (i *Image) AspectRatio() float64  { return i.Ratio }
@@ -14,7 +17,21 @@ func (i *Image) FixedWidth() *float64  { return i.Width }
 func (i *Image) FixedHeight() *float64 { return i.Height }
 
 func (i *Image) Render(ctx *RenderContext, x, y, width, height float64) error {
-	err := ctx.PDF.Image(i.Path, x, y, &gopdf.Rect{W: width, H: height})
+	if i.GetBytes == nil {
+		return errors.New("GetBytes function not defined")
+	}
+
+	bytes, err := i.GetBytes()
+	if err != nil {
+		return err
+	}
+
+	imageHolder, err := gopdf.ImageHolderByBytes(bytes)
+	if err != nil {
+		return err
+	}
+
+	err = ctx.PDF.ImageByHolder(imageHolder, x, y, &gopdf.Rect{W: width, H: height})
 	if err != nil {
 		return err
 	}
